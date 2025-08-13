fa87c693979ae6605fbe5d974b96b04e9aea6c40
lunabear <dongom910@gmail.com>
2025-08-13 18:34:26 +0900

    feat: v0 관련 링크를 새로운 Notion URL로 업데이트

--- lib/links.ts
+++ lib/links.ts
@@ -1,8 +1,8 @@
 // 외부 링크 관리
 export const EXTERNAL_LINKS = {
   // v0 관련 링크
-  V0_INTRODUCTION_GUIDE: 'https://gsholdings.notion.site/link1-24cf800bd1c180738c24fa11b1ac975d?source=copy_link',
-  V0_DOCUMENT_UPLOAD_GUIDE: 'https://gsholdings.notion.site/link2-24cf800bd1c1800095b9d25cead70944?source=copy_link',
+  V0_INTRODUCTION_GUIDE: 'https://www.notion.so/gsholdings/v0-24ef800bd1c1802abf33f71d0cdc6e02?v=247f800bd1c18151a819000cad02fcd8&source=copy_link#24ef800bd1c180579ad9f4a67ff8174c',
+  V0_DOCUMENT_UPLOAD_GUIDE: 'https://www.notion.so/gsholdings/v0-24ef800bd1c1802abf33f71d0cdc6e02?v=247f800bd1c18151a819000cad02fcd8&source=copy_link#24ef800bd1c180579ad9f4a67ff8174c',
   // MISO 관련 링크
   PROMPT_WRITING_GUIDE: 'https://miso-52g.gitbook.io/miso-manual/study/prompt',
   KNOWLEDGE_UPLOAD_GUIDE: 'https://miso-52g.gitbook.io/miso-manual/manual/6./build-knowledge',
